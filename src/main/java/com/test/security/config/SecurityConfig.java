e3590035a8e1f77313cfc21d4f5d5fb231e4ad8b
junseok-99 <angec5645@naver.com>
2024-07-21 21:48:43 +0900

    Initial commit

---
+++ src/main/java/com/test/security/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.test.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((csrfConfig) ->
+                        csrfConfig.disable()
+                )
+                .headers((headerConfig) ->
+                        headerConfig.frameOptions(frameOptionsConfig ->
+                                frameOptionsConfig.sameOrigin()
+                        )
+                )
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/test/security/util/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.test.security.util;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Slf4j
+@Component
+public class JwtUtil {
+
+    private final Key key;
+    private final long accessTokenExpirationTime;
+
+    public JwtUtil(
+            @Value("${jwt.secret") String secretKey,
+            @Value("${jwt.expiration_time}") long accessTokenExpirationTime
+    ) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.accessTokenExpirationTime = accessTokenExpirationTime;
+    }
+}
